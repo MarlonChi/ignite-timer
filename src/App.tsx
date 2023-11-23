c6495767612e616dc21e1774e7398529de679d91
MarlonChi <marlonchiodelli@hotmail.com>
2023-11-23 17:05:37 -0300

    feat: header

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from "react-router-dom";
 
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
-import { Router } from "./components/Router";
+import { Router } from "./Router";
 
 export function App() {
   return (
