b5e0044734b90986794f1249fbfe5bffbb8d967b
MarlonChi <marlonchiodelli@hotmail.com>
2023-11-22 21:29:08 -0300

    feat: Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { ThemeProvider } from "styled-components";
+import { BrowserRouter } from "react-router-dom";
 
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
+import { Router } from "./components/Router";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      Ignite Timer
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   );
